0bd438e7fdfa068a831bd5c8a8981720a1156c0d
Vivaan <vivaanmathur@outlook.com>
2022-05-30 22:06:29 +0530

    Add project files.

---
+++ ToDoFy/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ToDoFy;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
